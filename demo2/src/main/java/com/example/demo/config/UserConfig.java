d58c29a73ad15f7e9a27c1590140e17190173f20
geshuai <geshuai1992@163.com>
2020-11-24 01:02:11 +0800

    test

--- demo2/src/main/java/com/example/demo/config/UserConfig.java
+++ demo2/src/main/java/com/example/demo/config/UserConfig.java
@@ -4,7 +4,7 @@ import com.example.demo.entity.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration  //代表当前类是一个配置类，并不是业
+@Configuration  //代表当前类是一个配置类
 public class UserConfig {
 
     @Bean(name="user")   // 构建一个实例，并放在spring容器中, name默认是方法名，也可以手动指定
