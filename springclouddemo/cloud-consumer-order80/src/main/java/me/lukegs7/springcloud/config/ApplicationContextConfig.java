df45fb84097d310b9edce49fd30858f7b2257e3a
geshuai <geshuai1992@163.com>
2020-12-30 23:06:41 +0800

    feat: add common api

---
+++ springclouddemo/cloud-consumer-order80/src/main/java/me/lukegs7/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,13 @@
+package me.lukegs7.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationContextConfig {
+    @Bean
+    RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
