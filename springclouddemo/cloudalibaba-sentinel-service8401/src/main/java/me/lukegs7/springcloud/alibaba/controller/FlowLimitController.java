a664cd5e8d86c00a71b07f8b9c7dd9c0e47584ca
lukegs7 <geshuai1992@gmail.com>
2021-01-17 18:00:59 +0800

    feat: sentinel

---
+++ springclouddemo/cloudalibaba-sentinel-service8401/src/main/java/me/lukegs7/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,54 @@
+package me.lukegs7.springcloud.alibaba.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+
+@RestController
+@Slf4j
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA() {
+        return "------ testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        log.info(Thread.currentThread().getName() + "\t" + ".....testB");
+        return "------ testB, ";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        log.info("testD，异常比例");
+        int a = 10 / 0;
+        return "------ testD ";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+        log.info("testE，异常比例");
+        int a = 10 / 0;
+        return "------ testE ";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "dealTestHotKey") //value唯一即可
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        int a = 10 / 0;
+        return "---- testHotKey";
+    }
+
+    public String dealTestHotKey(String p1, String p2, BlockException exception) {
+        return "---- dealTestHotKey";
+    }
+
+}
